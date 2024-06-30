f16700ea21e6b5243792a44ed241ed1ed3603cde
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2024-06-30 23:15:18 +0300

    ssh energy

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#Using Puppet to make changes to the configuration file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
